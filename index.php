502c45b9d92b193645d6fc26ce95e1e110ec97b4
natalijakatauske <natalija.katauske@gmail.com>
2023-01-19 22:20:03 +0200

    first exercise is done

--- index.php
+++ index.php
@@ -7,20 +7,17 @@ include_once 'Manager.php';
 $employee = new Employee('John', 1000);
 echo $employee . PHP_EOL;
 echo $employee->__clone() . PHP_EOL;
-echo $employee->calculateSalary() . PHP_EOL; // trinti
 
 echo Employee::getTypeDescription() . PHP_EOL; //static
 
-$intern = new Intern('Peter', 700) .PHP_EOL;
-var_dump($intern);
+$intern = new Intern('Peter', 700);
 echo $intern . PHP_EOL;
-// echo $intern->calculateSalary() . PHP_EOL;
-// echo $intern->getTypeDescription() . PHP_EOL;
-// $cloneIntern = clone $inter;
-// print_r($cloneIntern);
+echo $intern->calculateSalary() . PHP_EOL;
+echo $intern->getTypeDescription() . PHP_EOL;
+$cloneIntern = clone $intern;
+print_r($cloneIntern);
 
-$manager = new Manager('Steve', 1200, 10) . PHP_EOL;
-var_dump($manager);
+$manager = new Manager('Steve', 1200, 10);
 echo $manager . PHP_EOL;
-// echo $manager->calculateSalary() . PHP_EOL;
-// echo $manager->getTypeDescription() . PHP_EOL;
+echo $manager->calculateSalary() . PHP_EOL;
+echo $manager->getTypeDescription() . PHP_EOL;
